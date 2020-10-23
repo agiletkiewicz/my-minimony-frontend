d0b350f012c2477255c1e0d192c687ce0d8775c1
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-22 21:25:14 -0700

    import redux into index file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
