1651bb7c8198bb88966cb5a0abe89a9e184690ff
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-26 13:59:19 -0700

    create posts container and component

---
+++ src/components/Posts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Posts extends React.Component {
+
+    render() {
+        return <div>Post</div>
+    }
+} 
+
+export default Posts;
---
+++ src/containers/postsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Posts from '../components/Posts'
+
+class PostsContainer extends React.Component {
+
+    render() {
+        return <div><Posts /></div>
+    }
+} 
+
+export default PostsContainer;
