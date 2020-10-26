abb00ce52023fef8ce6606bddedd3f0a3d703d4f
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-26 16:29:14 -0700

    render posts via Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Post(props) {
 
     return (
-        <div> {console.log(props.post)} </div>
+        <div> 
+            <a href={props.post.url}>
+                <img src={props.post.image_url} />
+            </a>
+            <h1>{props.post.title}</h1>
+            <p>{props.post.description}</p>
+        </div>
     )
 
 } 
