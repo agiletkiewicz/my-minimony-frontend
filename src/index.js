c370352d0e9226600af6126662560306ee3bd7b4
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-27 12:57:24 -0700

    set up bootstrap

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
