fdd491a4f0b19b115417e0801de56cc06c993f53
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-27 20:53:11 -0700

    display post information on post show

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
  
 const PostShow = props => {
  
   return (
-    <div>
-      <h3>{props.post.title}</h3>
-    </div>
+    <Container>
+        <Row>
+            <Col>
+                <Image src={props.post.image_url} fluid />
+            </Col>
+            <Col>
+                <h3>{props.post.title}</h3>
+                <p>{props.post.description}</p>
+                <a href={props.post.url} target="_blank">Go to website</a>
+            </Col>
+        </Row>
+    </Container>
   );
 }
  
