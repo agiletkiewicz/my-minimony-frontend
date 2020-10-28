a5964a6c48ed377c1159a3febd62ecc6dd100e01
agiletkiewicz <a.letkiewicz@gmail.com>
2020-10-27 22:42:43 -0700

    add home link to post show page

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -3,7 +3,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
-import Card from 'react-bootstrap/Card'
+import { Link } from 'react-router-dom';
  
 const PostShow = props => {
  
@@ -11,6 +11,13 @@ const PostShow = props => {
     
     <Container>
         <Row>
+            <Col xs={1}>
+                <Link to={"/"}>
+                <h2>{"<"}</h2>
+                </Link>
+                <br/>
+                home
+            </Col>
             <Col>
                 <Image src={props.post.image_url} fluid />
             </Col>
