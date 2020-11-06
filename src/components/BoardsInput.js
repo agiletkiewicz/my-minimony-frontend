9ff703c7b9e15dce8d9bbafc0f30501094cbf138
agiletkiewicz <a.letkiewicz@gmail.com>
2020-11-06 06:52:18 -0800

    add board input component

---
+++ src/components/BoardsInput.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl'
+import { connect } from 'react-redux';
+import { addBoard } from '../actions/addBoard';
+import Alert from 'react-bootstrap/Alert';
+
+class BoardsInput extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      title: '',
+     };
+  }
+
+handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  };
+
+handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.addBoard(this.state); 
+    this.setState({
+      title: '',
+    })
+  };
+
+render() {
+  const { title } = this.state
+
+  return (
+    <>
+        <Form inline onSubmit={this.handleSubmit}>
+          <FormControl 
+            type="text" 
+            placeholder="title" 
+            className="mr-sm-2" 
+            name="title"
+            value={title}
+            onChange={this.handleChange}
+          />
+          <Button variant="outline-success" type="submit">Add</Button>
+        </Form>
+        </>
+      );
+    }
+}
+
+
+export default connect(null, { addBoard })(BoardsInput);
