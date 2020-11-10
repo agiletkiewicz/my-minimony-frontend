d33bd446da849a913627e20a43cc67e89e192b97
agiletkiewicz <a.letkiewicz@gmail.com>
2020-11-09 22:51:21 -0800

    error alert is dismissable

--- src/components/Error.js
+++ src/components/Error.js
@@ -6,10 +6,17 @@ import Alert from 'react-bootstrap/Alert';
 
 class Error extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      show: true
+    }
+  }
+
   render() {
-    if (this.props.errors.length > 0) { 
+    if (this.props.errors.length > 0 && this.state.show) { 
         return (
-            <Alert inline variant={'danger'}>
+            <Alert inline variant={'danger'} onClose={() => this.setState({show: false})} dismissible>
                 {this.props.errors.map(error => <p>{error}</p>)}
             </Alert>
         );
