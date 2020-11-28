7894cca8dffba3e837264a82f495c92ac66243dc
agiletkiewicz <a.letkiewicz@gmail.com>
2020-11-27 16:39:11 -0800

    refactor error rendering to use react portal

--- src/components/Error.js
+++ src/components/Error.js
@@ -13,34 +13,6 @@ class Error extends React.Component {
   }
 
   render() {
-    
-    // if (this.props.errors.length > 0 && this.state.show) {
-    //   return (
-    //     <Alert
-    //       inline
-    //       variant="danger"
-    //       onClose={() => this.setState({ show: false })}
-    //       dismissible
-    //     >
-    //       {this.props.errors.map((error) => (
-    //         <p>{error}</p>
-    //       ))}
-    //     </Alert>
-    //   );
-    // }
-    // if (this.props.errors.length > 0) {
-    //   return (
-    //     <Button
-    //       onClick={() => this.setState({ show: true })}
-    //       size="sm"
-    //       variant="light"
-    //     >
-    //       Show Alert
-    //     </Button>
-    //   );
-    // }
-    // return null;
- 
       return (
         <Portal>
         <Alert
