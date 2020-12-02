cdb37916a5c6072b8d7adb72520e43dcabd32e51
agiletkiewicz <a.letkiewicz@gmail.com>
2020-12-02 14:29:20 -0800

    refactor remove save from class to functional component

--- src/components/posts/RemoveSaveButton.js
+++ src/components/posts/RemoveSaveButton.js
@@ -3,23 +3,22 @@ import Button from 'react-bootstrap/Button';
 import { connect } from 'react-redux';
 import { deleteSave } from '../../actions/deleteSave';
 
-class RemoveSaveButton extends React.Component {
-  handleClick = (event) => {
+function RemoveSaveButton(props) {
+
+  function handleClick(event) {
     event.preventDefault();
-    this.props.deleteSave(this.props.save.id);
+    props.deleteSave(props.save.id);
   };
 
-  render() {
     return (
       <>
         <br />
         <br />
-        <Button variant="light" onClick={this.handleClick}>
-          Remove from <b>{this.props.board.title}</b> board
+        <Button variant="light" onClick={handleClick}>
+          Remove from <b>{props.board.title}</b> board
         </Button>
       </>
-    );
-  }
+    )
 }
 
 export default connect(null, { deleteSave })(RemoveSaveButton);
