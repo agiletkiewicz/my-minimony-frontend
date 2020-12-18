30b1951935b646ef93967760c45d7f16adb302df
agiletkiewicz <a.letkiewicz@gmail.com>
2020-12-17 23:32:45 -0800

    add delete follow action

---
+++ src/actions/deleteFollow.js
@@ -0,0 +1,14 @@
+export const deleteFollow = (followId) => (dispatch) => {
+    fetch(`http://localhost:3000/api/v1/follows/${followId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((parsedResp) => dispatch({ type: 'DELETE_FoLLOW', follow: followId }))
+      .catch((error) =>
+        dispatch({ type: 'ADD_ERROR', error: 'Something went wrong. Try again.' })
+      );
+  };
+  
