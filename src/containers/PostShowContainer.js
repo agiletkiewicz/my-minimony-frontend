faa60004c22c7d9d596c929eda3568f14ca16f26
agiletkiewicz <a.letkiewicz@gmail.com>
2021-01-02 18:36:16 -0800

    fix deployed site intsead of local host

--- src/containers/PostShowContainer.js
+++ src/containers/PostShowContainer.js
@@ -13,7 +13,7 @@ class PostShowContainer extends Component {
     }
 
     componentDidMount() {
-        fetch(`http://localhost:3000/api/v1/posts/${this.props.post.id}/boards`)
+        fetch(`https://my-minimony.herokuapp.com/api/v1/posts/${this.props.post.id}/boards`)
             .then((resp) => resp.json())
             .then((response) => this.setState({ boards: response.data.map(response => response.attributes) }))
             .catch( () => this.props.dispatch({type: 'ADD_ERROR', error: "Something went wrong. Try again."}))
