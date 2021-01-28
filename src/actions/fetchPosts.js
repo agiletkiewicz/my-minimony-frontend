3ceca769446e91e01fef64f3f43387834a25d446
agiletkiewicz <a.letkiewicz@gmail.com>
2021-01-28 15:21:05 -0800

    add loading to posts fetch

--- src/actions/fetchPosts.js
+++ src/actions/fetchPosts.js
@@ -1,10 +1,12 @@
 export function fetchPosts() {
   return (dispatch) => {
-    fetch('https://my-minimony.herokuapp.com/api/v1/posts')
+    dispatch({ type: 'START_LOADING' });
+    fetch('http://localhost:3000/api/v1/posts')
       .then((resp) => resp.json())
       .then((parsedResp) => {
         dispatch({ type: 'FETCH_POSTS', posts: parsedResp });
         dispatch({ type: 'CLEAR_ERROR' });
+        dispatch({ type: 'FINISH_LOADING' });
       })
       .catch((error) =>
         dispatch({
