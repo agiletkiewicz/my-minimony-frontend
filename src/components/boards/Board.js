2e031c87f6d23de93312c87ef9fd9626524b0cad
agiletkiewicz <a.letkiewicz@gmail.com>
2021-02-03 19:09:23 -0700

    replace board cards with contianer

--- src/components/boards/Board.js
+++ src/components/boards/Board.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import { Link } from 'react-router-dom';
 
 function Board(props) {
   return (
     <div>
     <Link to={`/boards/${props.board.id}`}>
-      <Card>
+      {/* <Card>
         <Card.Body bsPrefix="board-heading">
           <Card.Text>{props.board.title}</Card.Text>
         </Card.Body>
         <Card.Img src={props.board.image} alt="Card image" />
-      </Card>
+      </Card> */}
+      <Container>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
     </Link>
     </div>
   );
